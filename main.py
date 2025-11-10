12e4bf1ddc981e3d522f7dc0a7c5658086c60d70
nozila2024 <nozilarajabboyeva@gmail.com>
2025-11-10 17:43:53 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#1-misol
+n = input("Ism kiriting: ")
+print("Bu ism",len(n), "ta harfdan iborat.")
+
+#2-misol
+n = "BU BERILGAN MATN"
+print(f"Matnning birinchi harfi: {n[0]}")
+print(f"Matnning oxirgi harfi: {n[-1]}")
+
+#3-misol
+n = input("Kichik harflar bilan matn kiriting: ")
+print(n.upper())
+
+#4-misol
+n = input("Katta harflar bilan matn kiriting: ")
+print(n.lower())
+
+#5-misol
+n = "  nozila & dilnura & sevinch  "
+print(n.strip())
+
+#6-misol
+n = "nozila & guljaxon"
+print(len(n))
